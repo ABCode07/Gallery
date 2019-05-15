56ed2fa7c7b4b7cf15f9170ca1026c93ece604ec
ABCode <abcode07@gmail.com>
2019-05-15 16:59:28 +0100

    Gallery
    Responsive Gallery Page

---
+++ Gallery/index.php
@@ -0,0 +1,117 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="icon" type="image/png" href="abb.png">
+	<title> Gallery </title>
+	<link rel="stylesheet" type="text/css" href="stylesheet.css">
+	<link rel="stylesheet" type="text/css" href="responsive.css">
+</head>
+<body>
+
+<!-- Nav Bar -->
+
+<nav>
+	
+<h2> Gallery </h2>
+
+<button id="btn"><svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="bars" class="svg-inline--fa fa-bars fa-w-14" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path fill="currentColor" d="M16 132h416c8.837 0 16-7.163 16-16V76c0-8.837-7.163-16-16-16H16C7.163 60 0 67.163 0 76v40c0 8.837 7.163 16 16 16zm0 160h416c8.837 0 16-7.163 16-16v-40c0-8.837-7.163-16-16-16H16c-8.837 0-16 7.163-16 16v40c0 8.837 7.163 16 16 16zm0 160h416c8.837 0 16-7.163 16-16v-40c0-8.837-7.163-16-16-16H16c-8.837 0-16 7.163-16 16v40c0 8.837 7.163 16 16 16z"></path></svg></button>
+
+<ul class="ul1" id="u1">
+	
+<li><a href="#"> Account </a></li>
+<li><a href="#"> Profile </a></li>
+<li><a href="#"> Log out </a></li>
+<li><a href="#"> Alert </a></li>
+
+</ul>
+
+<ul class="ul2" id="u2">
+	
+<li><svg aria-hidden="true" data-prefix="fab" data-icon="facebook" class="svg-inline--fa fa-facebook fa-w-14" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path fill="currentColor" d="M448 56.7v398.5c0 13.7-11.1 24.7-24.7 24.7H309.1V306.5h58.2l8.7-67.6h-67v-43.2c0-19.6 5.4-32.9 33.5-32.9h35.8v-60.5c-6.2-.8-27.4-2.7-52.2-2.7-51.6 0-87 31.5-87 89.4v49.9h-58.4v67.6h58.4V480H24.7C11.1 480 0 468.9 0 455.3V56.7C0 43.1 11.1 32 24.7 32h398.5c13.7 0 24.8 11.1 24.8 24.7z"></path></svg></li>
+
+<li><svg aria-hidden="true" data-prefix="fab" data-icon="twitter" class="svg-inline--fa fa-twitter fa-w-16" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path fill="currentColor" d="M459.37 151.716c.325 4.548.325 9.097.325 13.645 0 138.72-105.583 298.558-298.558 298.558-59.452 0-114.68-17.219-161.137-47.106 8.447.974 16.568 1.299 25.34 1.299 49.055 0 94.213-16.568 130.274-44.832-46.132-.975-84.792-31.188-98.112-72.772 6.498.974 12.995 1.624 19.818 1.624 9.421 0 18.843-1.3 27.614-3.573-48.081-9.747-84.143-51.98-84.143-102.985v-1.299c13.969 7.797 30.214 12.67 47.431 13.319-28.264-18.843-46.781-51.005-46.781-87.391 0-19.492 5.197-37.36 14.294-52.954 51.655 63.675 129.3 105.258 216.365 109.807-1.624-7.797-2.599-15.918-2.599-24.04 0-57.828 46.782-104.934 104.934-104.934 30.213 0 57.502 12.67 76.67 33.137 23.715-4.548 46.456-13.32 66.599-25.34-7.798 24.366-24.366 44.833-46.132 57.827 21.117-2.273 41.584-8.122 60.426-16.243-14.292 20.791-32.161 39.308-52.628 54.253z"></path></svg></li>
+
+<li><svg aria-hidden="true" data-prefix="fas" data-icon="envelope" class="svg-inline--fa fa-envelope fa-w-16" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path fill="currentColor" d="M502.3 190.8c3.9-3.1 9.7-.2 9.7 4.7V400c0 26.5-21.5 48-48 48H48c-26.5 0-48-21.5-48-48V195.6c0-5 5.7-7.8 9.7-4.7 22.4 17.4 52.1 39.5 154.1 113.6 21.1 15.4 56.7 47.8 92.2 47.6 35.7.3 72-32.8 92.3-47.6 102-74.1 131.6-96.3 154-113.7zM256 320c23.2.4 56.6-29.2 73.4-41.4 132.7-96.3 142.8-104.7 173.4-128.7 5.8-4.5 9.2-11.5 9.2-18.9v-19c0-26.5-21.5-48-48-48H48C21.5 64 0 85.5 0 112v19c0 7.4 3.4 14.3 9.2 18.9 30.6 23.9 40.7 32.4 173.4 128.7 16.8 12.2 50.2 41.8 73.4 41.4z"></path></svg></li>
+
+</ul>
+
+</nav>
+
+<!-- -->
+
+<!-- Content -->
+
+<section>
+	
+<div class="d"><div><img src="images/x (1).jpg"></div></div>
+<div class="d"><div><img src="images/x (2).jpg"></div></div>
+<div class="d"><div><img src="images/x (3).jpg"></div></div>
+<div class="d"><div><img src="images/x (4).jpg"></div></div>
+<div class="d"><div><img src="images/x (5).jpg"></div></div>
+<div class="d"><div><img src="images/x (6).jpg"></div></div>
+<div class="d"><div><img src="images/x (7).jpg"></div></div>
+<div class="d"><div><img src="images/x (8).jpg"></div></div>
+<div class="d"><div><img src="images/x (9).jpg"></div></div>
+<div class="d"><div><img src="images/x (10).jpg"></div></div>
+<div class="d"><div><img src="images/x (11).jpg"></div></div>
+<div class="d"><div><img src="images/x (12).jpg"></div></div>
+<div class="d"><div><img src="images/x (13).jpg"></div></div>
+<div class="d"><div><img src="images/x (14).jpg"></div></div>
+<div class="d"><div><img src="images/x (15).jpg"></div></div>
+<div class="d"><div><img src="images/x (16).jpg"></div></div>
+<div class="d"><div><img src="images/x (17).jpg"></div></div>
+<div class="d"><div><img src="images/x (18).jpg"></div></div>
+<div class="d"><div><img src="images/x (19).jpg"></div></div>
+<div class="d"><div><img src="images/x (20).jpg"></div></div>
+<div class="d"><div><img src="images/x (21).jpg"></div></div>
+<div class="d"><div><img src="images/x (22).jpg"></div></div>
+<div class="d"><div><img src="images/x (23).jpg"></div></div>
+<div class="d"><div><img src="images/x (24).jpg"></div></div>
+<div class="d"><div><img src="images/x (25).jpg"></div></div>
+<div class="d"><div><img src="images/x (26).jpg"></div></div>
+<div class="d"><div><img src="images/x (27).jpg"></div></div>
+<div class="d"><div><img src="images/x (28).jpg"></div></div>
+<div class="d"><div><img src="images/x (29).jpg"></div></div>
+<div class="d"><div><img src="images/x (30).jpg"></div></div>
+<div class="d"><div><img src="images/x (31).jpg"></div></div>
+<div class="d"><div><img src="images/x (32).jpg"></div></div>
+
+</section>
+
+<!-- -->
+
+<!-- Footer -->
+
+<footer></footer>
+
+<!-- -->
+
+<script src="jquery.js"></script>
+<script>
+
+var i=0;
+
+$("#btn").click( function() {
+
+i++;
+
+if (i % 2 == 1) {
+
+$("#u1").css("display","block");
+$("#u2").css("display","block");
+
+}else if (i % 2 ==0) {
+
+$("#u1").css("display","none");
+$("#u2").css("display","none");
+
+}
+
+});
+
+</script>
+
+</body>
+</html>
